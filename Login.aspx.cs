5a5190816b0c748a6399d0a0156b5c3831b8fd4a
denizyildizz <48515499+denizyildizz@users.noreply.github.com>
2019-03-27 22:22:33 +0300

    ıteration 2

---
+++ Login.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        LinkButton lbMasterLogin = new LinkButton();
+        lbMasterLogin = (LinkButton)Master.FindControl("LinkButtonLogin");
+        lbMasterLogin.Visible = false;
+    }
+
+    protected void ButtonSubmit_Click(object sender, EventArgs e)
+    {
+
+        try
+        {
+            LabelResult.Visible = false;
+
+            using (var myDB = new c382atsEntities())
+            {
+                TblUser loginuser = (from u in myDB.TblUsers
+                                  where u.email == TextBoxEmail.Text &&
+                                  u.pass == TextBoxPass.Text && u.isActive == true
+                                  select u).SingleOrDefault();
+
+                if (loginuser != null)
+                {
+                    Session["user"] = loginuser;
+                    Response.Redirect("Default");
+
+                }
+                else
+                {
+
+                    LabelResult.Text = "Wrong!!!";
+                    LabelResult.Visible = true;
+                }
+
+            }
+
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+
+    }
+}
---
+++ Register.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        LinkButton lbMasterRegister = new LinkButton();
+        lbMasterRegister = (LinkButton)Master.FindControl("LinkButtonRegister");
+        lbMasterRegister.Visible = false;
+
+    }
+    protected void ButtonSubmit_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            using (var myDB = new c382atsEntities())
+            {
+                var checkuser = (from u in myDB.TblUsers
+                                 where u.email == TextBoxEmail.Text
+                                 select u).SingleOrDefault();
+
+                if (checkuser == null)
+                {
+                    LabelError.Visible = false;
+                    var newuser = new TblUser();
+
+                    newuser.title = DDLTitle.SelectedItem.Text;
+                    newuser.fname = TextBoxFname.Text;
+                    newuser.lname = TextBoxLastName.Text;
+                    newuser.email = TextBoxEmail.Text;
+                    newuser.pass = TextBoxPass.Text;
+                    newuser.timestamp = DateTime.Now;
+                    newuser.isActive = true;
+                    newuser.isAdmin = false;
+                    myDB.TblUsers.Add(newuser);
+                    myDB.SaveChanges();
+                    LabelError.Visible = false;
+                    Response.Redirect("Login");
+                }
+                else
+                {
+                    LabelError.Visible = true;
+                }
+
+            }
+        }
+        catch (Exception)
+        {
+            LabelException.Visible = true;
+            throw;
+        }
+    }
+
+}
