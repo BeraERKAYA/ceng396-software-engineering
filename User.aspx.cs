bafc90d974265fe7eb55a884d0ccabab2ea52ad8
nisancandir <40786928+nisancandir@users.noreply.github.com>
2019-05-31 15:32:39 +0300

    Update User.aspx.cs

--- User.aspx.cs
+++ User.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿<!--this page is a backend page for User.aspx coded with c#-->
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,28 +13,27 @@ public partial class User : System.Web.UI.Page
 
         using (var myDB = new tryEntities())
         {
-            var a = Convert.ToInt32(Session["UserId"]);
+            var a = Convert.ToInt32(Session["UserId"]);//get user id from session
             var b = (from u in myDB.users
                      where u.user_id == a
                      select u).Single();
             var c = (from x in myDB.levels
-                     where x.user_id == a
+                     where x.user_id == a && x.level_num=10
                      select x).ToList();
-            var usercertif = (from f in myDB.levels
-                              where f.user_id == cu_id && f.level_num == 10
-                              select f).Single();
-            if (usercertif.isSolved == true)
-            {
-                Label30.Visible = true;
-
-
+            if (b.totalscore>=70 &&  c.isSolved==true){//check if user total score is %70 and user solved 10th quiz if it is true;
+            Certificate cer= new Certifictae();
+            cer.user_id=a;
+            myDB.Certificates.Add(cer);
+            myDB.SaveChanges();//give user certificate and store in database
+            Label30.Visible = true;//make it available for user to see his/her certificate
             }
+            
 
             Label1.Text = b.fname;
             Label2.Text = b.lname;
             Label3.Text = b.username;
             Label4.Text = Convert.ToString( b.totalscore);
-            foreach(var y in c)
+            foreach(var y in c)//shows each level(10) score to user gotten from database
             {
                 if(y.level_num==1 && y.isSolved==true)
                 {
@@ -112,9 +112,9 @@ public partial class User : System.Web.UI.Page
         }
     }
 
-    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)//ıf user clicks log out button ;
     {
-        Session.Abandon();
-        Response.Redirect("Login.aspx");
+        Session.Abandon();//delete sessions
+        Response.Redirect("Login.aspx");//direct to login page
     }
 }
