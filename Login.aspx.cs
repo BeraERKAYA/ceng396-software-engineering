1a52e215c8d83562e63f9ee631cd9f6e67c1210b
nisancandir <40786928+nisancandir@users.noreply.github.com>
2019-05-31 15:11:22 +0300

    Update Login.aspx.cs

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//this is a backend page for Login.aspx page coded with c#
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,19 +19,19 @@ public partial class Login : System.Web.UI.Page
         {
             var loggeduser = (from u in myDB.users
                               where u.username == TextBox1.Text
-                              select u).Single();
+                              select u).Single();//get user info. from databse
             if (loggeduser != null)
             {
-                Session["UserId"] = loggeduser.user_id;
+                Session["UserId"] = loggeduser.user_id;//store user id in session
                
 
-                Response.Redirect("ListLevels.aspx");
+                Response.Redirect("ListLevels.aspx");//direct tolist of levels page
 
 
             }
             else
             {
-                Response.Redirect("Register.aspx");
+                Response.Redirect("Register.aspx");//if user username did not found dirct user to register page
             }
 
         }
