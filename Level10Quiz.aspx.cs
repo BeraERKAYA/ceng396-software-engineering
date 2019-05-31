f15ea4ce0dc12d8e9a0647feb931751a5a22d761
nisancandir <40786928+nisancandir@users.noreply.github.com>
2019-05-31 14:55:36 +0300

    Update Level10Quiz.aspx.cs

--- Level10Quiz.aspx.cs
+++ Level10Quiz.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+//this page is backend of Level10Quiz.aspx page coded with c#
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,14 +12,14 @@ public partial class Level10Quiz : System.Web.UI.Page
     {
         using (var myDB = new tryEntities())
         {
-            var a = Convert.ToInt32(Session["UserId"]);
+            var a = Convert.ToInt32(Session["UserId"]);//get current user's id stored in session
             var b = (from u in myDB.lessons
-                     where u.user_id == a && u.level_num == 6
-                     select u).Single();
+                     where u.user_id == a && u.level_num == 9
+                     select u).Single();//get lesson 9 information
 
-            if (b.isRead != true)
+            if (b.isRead != true)//if user did not read lesson 9
             {
-                Response.Redirect("ListLevels.aspx");
+                Response.Redirect("ListLevels.aspx");//user can not aceess to the page direct to ListLevels.aspx page
 
             }
 
@@ -26,7 +27,7 @@ public partial class Level10Quiz : System.Web.UI.Page
         }
     }
 
-    protected void Button1_Click(object sender, EventArgs e)
+    protected void Button1_Click(object sender, EventArgs e)//when user click submit button;
     {
 
         using (var myDB = new tryEntities())
@@ -43,75 +44,75 @@ public partial class Level10Quiz : System.Web.UI.Page
             var k = TextBox10.Text;
             var answerslist = (from u in myDB.questions
                                where u.quiz_num == 10
-                               select u).ToList();
+                               select u).ToList();//get questions information from databse
             var levelscore = 0;
-            foreach (var y in answerslist)
+            foreach (var y in answerslist)//compare user answer with true answer gotten from databse
             {
                 if (y.question_num == 1)
                 {
                     if (a == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 1 increase question answer
                 }
                 if (y.question_num == 2)
                 {
                     if (b == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 2 increase question answer
                 }
                 if (y.question_num == 3)
                 {
                     if (c == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 3 increase question answer
                 }
                 if (y.question_num == 4)
                 {
                     if (d == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 4 increase question answer
                 }
                 if (y.question_num == 5)
                 {
                     if (g == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 5 increase question answer
                 }
                 if (y.question_num == 6)
                 {
                     if (f == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 6 increase question answer
                 }
                 if (y.question_num == 7)
                 {
                     if (h == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 7 increase question answer
                 }
                 if (y.question_num == 8)
                 {
                     if (ı == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 8 increase question answer
                 }
                 if (y.question_num == 9)
                 {
                     if (j == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 9 increase question answer
                 }
                 if (y.question_num == 10)
                 {
                     if (k == y.question_answer)
-                        levelscore++;
+                        levelscore++;//if user answer is true on question number 10 increase question answer
                 }
             }
             var x = Convert.ToInt32(Session["UserId"]);
             var changescore = (from o in myDB.levels
                                where o.user_id == x && o.level_num == 10
-                               select o).Single();
-            changescore.score = levelscore;
-            changescore.isSolved = true;
-            myDB.SaveChanges();
+                               select o).Single();//get quiz 10 information from database
+            changescore.score = levelscore;//update level score of current user
+            changescore.isSolved = true;//update as solved
+            myDB.SaveChanges();//save the changes
             var User = (from o in myDB.users
                         where o.user_id == x
-                        select o).Single();
-            User.totalscore = User.totalscore + levelscore;
+                        select o).Single();//get user info from database
+            User.totalscore = User.totalscore + levelscore;//update user total score
 
 
-            myDB.SaveChanges();
+            myDB.SaveChanges();//save database changes
 
         }
     }
