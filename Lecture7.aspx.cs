9bfb66daec6810b5a90f4317fef2fd85ce38c0d2
nisancandir <40786928+nisancandir@users.noreply.github.com>
2019-05-31 14:29:58 +0300

    Update Lecture7.aspx.cs

--- Lecture7.aspx.cs
+++ Lecture7.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//this page is backend of Lecture7.aspx page coded with c#
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,35 +12,35 @@ public partial class Lecture7 : System.Web.UI.Page
     {
         using (var myDB = new tryEntities())
         {
-            var a = Convert.ToInt32(Session["UserId"]);
+            var a = Convert.ToInt32(Session["UserId"]);//get current user's id stored in session
             var b = (from u in myDB.lessons
                      where u.user_id == a && u.level_num == 6
-                     select u).Single();
+                     select u).Single();//get lecture 6 information from database
             var c = (from x in myDB.levels
                      where x.user_id == a && x.level_num == 6
-                     select x).Single();
-            if (b.isRead != true && c.isSolved != true)
+                     select x).Single();//get quiz 6 information from database
+            if (b.isRead != true && c.isSolved != true))//check if lecture 6 is readed and quiz 6 is Solved
             {
-                Response.Redirect("ListLevels.aspx");
+                Response.Redirect("ListLevels.aspx");//if not solved direct user to ListLevels.aspx page,user can not access this page yet
 
             }
 
 
         }
     }
-    protected void Button1_Click(object sender, EventArgs e)
+    protected void Button1_Click(object sender, EventArgs e)//when user clicks Done reading Lecture 7 Button;
     {
         using (var myDB = new tryEntities())
         {
             var a = Convert.ToInt32(Session["UserId"]);
             var currentuserlesson = (from u in myDB.lessons
                                      where u.user_id == a && u.level_num == 7
-                                     select u).Single();
-            currentuserlesson.isRead = true;
-            myDB.SaveChanges();
+                                     select u).Single();//get lecture 7 info from database
+            currentuserlesson.isRead = true;//update lecture 10 of that user as solved 
+            myDB.SaveChanges();//save database changes
 
         }
-        Response.Redirect("ListLevels.aspx");
+        Response.Redirect("ListLevels.aspx");//direct user to ListLevels.aspx page
        
 
     }
